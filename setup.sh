efb4c4aaf9e4541ec0a627e39b9ce3850afd236f
Nick Fox-Gieg <nick@fox-gieg.com>
2023-07-24 10:59:48 -0400

    shell

--- setup.sh
+++ setup.sh
@@ -13,7 +13,8 @@ cd $DIR
 git submodule init
 git submodule update --init --recursive
 git submodule sync
-git submodule foreach git checkout master
+git submodule foreach git checkout master 2>/dev/null
+git submodule foreach git checkout main 2>/dev/null
 git submodule foreach git reset --hard
-git submodule foreach git pull origin master
-
+git submodule foreach git pull origin master 2>/dev/null
+git submodule foreach git pull origin main 2>/dev/null
